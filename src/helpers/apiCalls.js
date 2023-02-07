e399221ab39ed3963722930b4e6bc80dc391b1ed
unknown <eric.bryant0@gmail.com>
2023-02-07 10:39:52 -0500

    updated api call url

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: `https://spootie-gen-backend.onrender.com/`,
+  baseURL: `https://spootie-gen-api.ericmbryant.com/`,
   withCredentials: false,
   headers: {
     Accept: "application/json",
